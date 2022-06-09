92b16e170c51c554b3626aee7bc99055194dcf77
p0tt3r-iOS <france998811@gmail.com>
2022-06-09 22:26:53 +0900

    feat: implement iterator_traits

--- iterator.hpp
+++ iterator.hpp
@@ -1,11 +1,29 @@
 #if !defined(ITERATOR_HPP)
 #define ITERATOR_HPP
 
-template <class Iterator>
-class iterator_traits {};
-template <class T>
-class iterator_traits<T *> {};
-template <class T>
-class iterator_traits<const T *> {};
+namespace ft {
+template <class Iter> class iterator_traits {
+  typedef typename Iter::difference_type difference_type;
+  typedef typename Iter::value_type value_type;
+  typedef typename Iter::pointer pointer;
+  typedef typename Iter::reference reference;
+  typedef typename Iter::iterator_category iterator_category;
+};
 
-#endif  // ITERATOR_HPP
+template <class T> class iterator_traits<T *> {
+  typedef ptrdiff_t difference_type;
+  typedef T value_type;
+  typedef T *pointer;
+  typedef T &reference;
+  typedef typename std::random_access_iterator_tag iterator_category;
+};
+template <class T> class iterator_traits<const T *> {
+  typedef ptrdiff_t difference_type;
+  typedef T value_type;
+  typedef const T *pointer;
+  typedef const T &reference;
+  typedef typename std::random_access_iterator_tag iterator_category;
+};
+} // namespace ft
+
+#endif // ITERATOR_HPP
