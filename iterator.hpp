aa385f37d2e23b2ce532e7bb03ebd088629fb8fa
p0tt3r-iOS <france998811@gmail.com>
2022-06-10 22:23:34 +0900

    feat: init reverse_iterator

--- iterator.hpp
+++ iterator.hpp
@@ -2,7 +2,8 @@
 #define ITERATOR_HPP
 
 namespace ft {
-template <class Iter> class iterator_traits {
+template <class Iter>
+class iterator_traits {
   typedef typename Iter::difference_type difference_type;
   typedef typename Iter::value_type value_type;
   typedef typename Iter::pointer pointer;
@@ -10,20 +11,42 @@ template <class Iter> class iterator_traits {
   typedef typename Iter::iterator_category iterator_category;
 };
 
-template <class T> class iterator_traits<T *> {
+template <class T>
+class iterator_traits<T *> {
   typedef ptrdiff_t difference_type;
   typedef T value_type;
   typedef T *pointer;
   typedef T &reference;
   typedef typename std::random_access_iterator_tag iterator_category;
 };
-template <class T> class iterator_traits<const T *> {
+
+template <class T>
+class iterator_traits<const T *> {
   typedef ptrdiff_t difference_type;
   typedef T value_type;
   typedef const T *pointer;
   typedef const T &reference;
   typedef typename std::random_access_iterator_tag iterator_category;
 };
-} // namespace ft
 
-#endif // ITERATOR_HPP
+template <class _Iterator>
+class reverse_iterator
+    : public iterator<typename iterator_traits<_Iterator>::iterator_category,
+                      typename iterator_traits<_Iterator>::value_type
+                      typename iterator_traits<_Iterator>::difference_type,
+                      typename iterator_traits<_Iterator>::pointer,
+                      typename iterator_traits<_Iterator>::reference> {
+ protected:
+  _Iterator current;
+
+  typedef iterator_traits<_Iterator> _traits_type;
+
+ public:
+  typedef _Iterator iterator_type;
+  typedef typename __traits_type::pointer pointer;
+  typedef typename __traits_type::difference_type difference_type;
+  typedef typename __traits_type::reference reference;
+};
+}  // namespace ft
+
+#endif  // ITERATOR_HPP
