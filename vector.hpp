c917c3446d73301780ffa04d7369827e5c928f4b
p0tt3r-iOS <france998811@gmail.com>
2022-06-15 20:19:20 +0900

    feat: add vector_base constructors

--- vector.hpp
+++ vector.hpp
@@ -6,8 +6,11 @@
 
 #include "iterator.hpp"
 
+#define _NOEXECPT throw()
+
 namespace ft {
 
+// vector_base
 template <typename _T, typename _Allocator>
 class __vector_base {
  public:
@@ -25,10 +28,51 @@ class __vector_base {
   typedef pointer                                  iterator;
   typedef const_pointer                            const_iterator;
 
-  pointer                                    __begin_;
-  pointer                                    __end_;
-  __compressed_pair<pointer, allocator_type> __end_cap_;
+  pointer        __begin_;
+  pointer        __end_;
+  pointer        __end_cap_pointer_;
+  allocator_type __end_cap_alloc_type_;
+
+  __vector_base() _NOEXCEPT;
+  __vector_base(const allocator_type& __a);
+  ~__vector_base();
+
+  allocator_type&       __alloc() _NOEXCEPT { return __end_cap_alloc_type_; }
+  const allocator_type& __alloc() const _NOEXCEPT {
+    return __end_cap_alloc_type_;
+  }
+  size_type capacity() const _NOEXCEPT {
+    return static_cast<size_type>(__end_cap_pointer_ - __begin_);
+  }
+  void clear() _NOEXCEPT {
+    pointer __tmp_end = __end_;
+    while (__begin_ != __tmp_end)
+      __alloc_traits::destroy(__alloc(), _VSTD::__to_address(--__tmp_end));
+    __end_ = __begin_;
+  }
 };
+
+template <typename _T, typename _Allocator>
+__vector_base<_T, _Allocator>::__vector_base() _NOEXCEPT
+    : __begin_(nullptr),
+      __end_(nullptr),
+      __end_cap_pointer_(nullptr),
+      __end_cap_alloc_type_(__default_init_tag()) {}
+
+template <typename _T, typename _Allocator>
+__vector_base<_T, _Allocator>::__vector_base(const allocator_type& __a)
+    : __begin_(nullptr),
+      __end_(nullptr),
+      __end_cap_pointer_(nullptr),
+      __end_cap_alloc_type_(__a) {}
+
+template <class _T, class _Allocator>
+__vector_base<_T, _Allocator>::~__vector_base() {
+  if (__begin_ != nullptr) {
+    __alloc_traits::deallocate(__alloc(), __begin_, capacity());
+  }
+}
+
 }  // namespace ft
 
 #endif  // VECTOR_HPP
