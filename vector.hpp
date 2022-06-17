13bd05eb008723af4ce5bb29bc896c64b01a7f64
p0tt3r-iOS <france998811@gmail.com>
2022-06-17 18:14:30 +0900

    feat: create vector frame

--- vector.hpp
+++ vector.hpp
@@ -80,6 +80,9 @@ class vector : private __vector_base<_T, _Allocator> {
   typedef __vector_base<_T, _Allocator> _base;
   typedef vector<_T, _Allocator>        vector_type;
 
+  iterator       __make_iter(pointer __p) { return iterator(__p); }
+  const_iterator __make_iter(const_pointer __p) { return const_iterator(__p); }
+
  public:
   typedef _T                                            value_type;
   typedef value_type                                   *pointer;
@@ -90,6 +93,63 @@ class vector : private __vector_base<_T, _Allocator> {
   typedef const value_type                             &const_reference;
   typedef size_t                                        size_type;
   typedef ptrdiff_t                                     difference_type;
+  typedef typename _base::allocator_type                allocator_type;
+  typedef reverse_iterator<const_iterator>              const_reverse_iterator;
+  typedef reverse_iterator<iterator>                    reverse_iterator;
+
+  explicit vector(const allocator_type &alloc = allocator_type());
+  explicit vector(size_type n, const value_type &val = value_type(),
+                  const allocator_type &alloc = allocator_type());
+  template <class InputIterator>
+  vector(InputIterator first, InputIterator last,
+         const allocator_type &alloc = allocator_type());
+  vector(const vector &x);
+
+  ~vector();
+
+  vector &operator=(const vector &x);
+
+  iterator       begin();
+  const_iterator begin() const;
+  iterator       end();
+  const_iterator end() const;
+
+  reverse_iterator       rbegin();
+  const_reverse_iterator rbegin() const;
+  reverse_iterator       rend();
+  const_reverse_iterator rend() const;
+
+  size_type size() const;
+  void      resize(size_type n, value_type val = value_type());
+  size_type max_size() const;
+  size_type capacity() const;
+  bool      empty() const;
+  void      reserve(size_type n);
+
+  reference       operator[](size_type n);
+  const_reference operator[](size_type n) const;
+  reference       at(size_type n);
+  const_reference at(size_type n) const;
+
+  reference       front();
+  const_reference front() const;
+  reference       back();
+  const_reference back() const;
+
+  template <class InputIterator>
+  void assign(InputIterator first, InputIterator last);
+  void assign(size_type n, const value_type &val);
+  void push_back(const value_type &val);
+  template <class InputIterator>
+  void     insert(iterator position, InputIterator first, InputIterator last);
+  iterator insert(iterator position, const value_type &val);
+  void     insert(iterator position, size_type n, const value_type &val);
+  iterator erase(const_iterator position);
+  iterator erase(const_iterator first, const_iterator last);
+  void     swap(vector &x);
+  void     clear() noexcept;
+
+  allocator_type get_allocator() const { return _base::__alloc(); }
 };
 
 }  // namespace ft
