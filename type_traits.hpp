1993bf97fe52ead65901d62e9105cf8494a6a0bd
p0tt3r-iOS <france998811@gmail.com>
2022-06-19 21:53:35 +0900

    feat: init type_traits

---
+++ type_traits.hpp
@@ -0,0 +1,249 @@
+#if !defined(TYPE_TRAITS_HPP)
+#define TYPE_TRAITS_HPP
+
+namespace ft {
+
+struct __true_type {};
+struct __false_type {};
+
+template <class _T>
+struct __type_traits {
+  typedef __true_type  this_dummy_member_must_be_first;
+  typedef __false_type has_trivial_default_constructor;
+  typedef __false_type has_trivial_copy_constructor;
+  typedef __false_type has_trivial_assignment_operator;
+  typedef __false_type has_trivial_destructor;
+  typedef __false_type is_POD_type;
+};
+
+template <>
+struct __type_traits<bool> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<char> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<signed char> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<unsigned char> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<wchar_t> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<short> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<unsigned short> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<int> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<unsigned int> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<long> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<unsigned long> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<long long> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<unsigned long long> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<float> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<double> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <>
+struct __type_traits<long double> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <class _T>
+struct __type_traits<_T*> {
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_operator;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template <class _Tp>
+struct _Is_integer {
+  typedef __false_type _Integral;
+};
+
+template <>
+struct _Is_integer<bool> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<char> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<signed char> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<unsigned char> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<wchar_t> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<short> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<unsigned short> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<int> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<unsigned int> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<long> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<unsigned long> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<long long> {
+  typedef __true_type _Integral;
+};
+
+template <>
+struct _Is_integer<unsigned long long> {
+  typedef __true_type _Integral;
+};
+
+template <typename _Tp>
+struct _Is_normal_iterator {
+  typedef __false_type _Normal;
+};
+
+}  // namespace ft
+
+#endif  // TYPE_TRAITS_HPP
