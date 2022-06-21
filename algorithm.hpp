c5324a822f1bc26496c4d76caff035b4277a5a7e
p0tt3r-iOS <france998811@gmail.com>
2022-06-21 21:56:49 +0900

    feat: add __copy_backward

--- algorithm.hpp
+++ algorithm.hpp
@@ -11,6 +11,13 @@ _T* __copy_trivial(const _T* __first, const _T* __last, _T* __result) {
   return __result + (__last - __first);
 }
 
+template <typename _T>
+_T* __copy_backward(const _T* __first, const _T* __last, _T* __result) {
+  const ptrdiff_t __n = __last - __first;
+  memmove(__result - __n, __first, sizeof(_T) * __n);
+  return __result - __n;
+}
+
 // ================================================================
 // Construct
 template <class _T1, class _T2>
@@ -20,7 +27,7 @@ void _Construct(_T1* __p, const _T2& __value) {
 
 template <class _T1>
 void _Construct(_T1* __p) {
-  new (static_cast<void*>(__p) _T1();)
+  new (static_cast<void*>(__p)) _T1();
 }
 
 }  // namespace ft
